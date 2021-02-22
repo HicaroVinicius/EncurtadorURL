90904a02eee256c50b01f158c8faf5dfee9f036f
HicaroVinicius <hicaro_vinicius@hotmail.com>
2021-02-22 18:35:26 -0300

    Update dbconnector.ts

--- src/dbconfig/dbconnector.ts
+++ src/dbconfig/dbconnector.ts
@@ -3,5 +3,6 @@ import { Pool } from 'pg';
 export default new Pool ({
     max: 20,
     connectionString: process.env.DATABASE_URL || 'postgres://postgres:123456@localhost:5432/encurtador',
-    idleTimeoutMillis: 30000
+    idleTimeoutMillis: 30000,
+    ssl: true
 });
